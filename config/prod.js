2a31f099ff947df68b0ae4f221d715e345630b0f
SolarZen <dirk_kunze@outlook.com>
2024-07-20 18:30:05 +1000

    added OAuth

---
+++ config/prod.js
@@ -0,0 +1,7 @@
+// Prod Keys
+module.exports = {
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  goggleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  mongoURI: process.env.MONGO_URI,
+  cookieKey: process.env.COOKIE_KEY,
+};
---
+++ services/passport.js
@@ -0,0 +1,38 @@
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const mongoose = require("mongoose");
+const keys = require("../config/keys");
+
+const User = mongoose.model("users");
+
+passport.serializeUser((user, done) => {
+  done(null, user.id); // user model id
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id).then((user) => done(null, user));
+});
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.goggleClientSecret,
+      callbackURL: "/auth/google/callback",
+    },
+    (accessToken, refreshToken, profile, done) => {
+      User.findOne({ googleID: profile.id }).then((existingUser) => {
+        if (existingUser) {
+          // User already exists
+          done(null, existingUser);
+        } else {
+          // Add User
+          new User({ googleID: profile.id })
+            .save()
+            // promise callback
+            .then((user) => done(done, user));
+        }
+      });
+    }
+  )
+);
