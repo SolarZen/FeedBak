4c6ad222c6c6060d7fdde0a81d3a82741a77dc7f
SolarZen <dirk_kunze@outlook.com>
2024-07-20 21:20:30 +1000

    test

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,7 @@
-if (process.env.NODE_ENV === "production") {
-  // return prod
-  module.exports = require("./prod");
-} else {
-  // return dev
-  module.exports = require("./dev");
-}
+// if (process.env.NODE_ENV === "production") {
+// return prod
+module.exports = require("./prod");
+// } else {
+//   // return dev
+//   module.exports = require("./dev");
+// }
